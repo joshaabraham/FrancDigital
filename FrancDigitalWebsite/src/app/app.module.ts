6d5be1cfd582410fe6896f4df35b35bd8de07b26
rems <duplan.remi@gmail.com>
2021-08-05 17:52:16 -0400

    using first module into app module

--- FrancDigitalWebsite/src/app/app.module.ts
+++ FrancDigitalWebsite/src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LeftColumnComponent } from './pages/First/left-column/left-column.component';
-import { MiddleColumnComponent } from './pages/First/middle-column/middle-column.component';
-import { RightColumnComponent } from './pages/First/right-column/right-column.component';
+import { FirstModule } from './pages/First/first/first.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftColumnComponent,
-    MiddleColumnComponent,
-    RightColumnComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FirstModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrancDigitalWebsite/src/app/pages/First/first/first.module.ts
+++ FrancDigitalWebsite/src/app/pages/First/first/first.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstComponent } from '../first/first.component';
+import { RightColumnComponent } from '../right-column/right-column.component';
+import { LeftColumnComponent } from '../left-column/left-column.component';
+import { MiddleColumnComponent } from '../middle-column/middle-column.component';
 
 
 
 @NgModule({
-  declarations: [
-    FirstComponent
+  declarations: [FirstComponent, RightColumnComponent, LeftColumnComponent, MiddleColumnComponent
   ],
+  exports:[FirstComponent, RightColumnComponent, LeftColumnComponent, MiddleColumnComponent],
   imports: [
-    CommonModule
+    CommonModule,   
   ]
 })
 export class FirstModule { }
