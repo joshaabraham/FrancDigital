130d7e670d8ed8c36f7d0c87d63d025f178c4b1c
rems <duplan.remi@gmail.com>
2021-08-05 15:11:12 -0400

    added components and coins file

--- FrancDigitalWebsite/src/app/app.module.ts
+++ FrancDigitalWebsite/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeftColumnComponent } from './pages/First/left-column/left-column.component';
+import { MiddleColumnComponent } from './pages/First/middle-column/middle-column.component';
+import { RightColumnComponent } from './pages/First/right-column/right-column.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftColumnComponent
+    LeftColumnComponent,
+    MiddleColumnComponent,
+    RightColumnComponent,
   ],
   imports: [
     BrowserModule,
---
+++ FrancDigitalWebsite/src/app/pages/First/middle-column/middle-column.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MiddleColumnComponent } from './middle-column.component';
+
+describe('MiddleColumnComponent', () => {
+  let component: MiddleColumnComponent;
+  let fixture: ComponentFixture<MiddleColumnComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MiddleColumnComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MiddleColumnComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
