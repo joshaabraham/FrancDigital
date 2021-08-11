e85a80f201227a6be8142165e69c6f21b5688adb
rems <duplan.remi@gmail.com>
2021-08-10 20:04:17 -0400

    first default route to first page working

--- FrancDigitalWebsite/src/app/app-routing.module.ts
+++ FrancDigitalWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstModule } from './pages/First/first/first.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/First/first/first.module').then(m => m.FirstModule) },
+ // { path: '', loadChildren: () => import('../pages/First/first/FirstModule').then(m => m.FirstModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
